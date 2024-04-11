1aef29a924da729a81120427fd4d4a6ea96f4079
andruha55533 <110409741+andruha55533@users.noreply.github.com>
2024-04-11 13:09:04 +0300

    Add files via upload

---
+++ go1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream> 
+#include <Windows.h>
+
+void printarr(int arr[],int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+    }
+}
+
+void bubble(int arr[],int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                arr[j] = arr[j] + arr[j + 1];
+                arr[j + 1] = arr[j] - arr[j + 1];
+                arr[j] = arr[j] - arr[j + 1];
+            }
+        }
+    }
+}
+
+
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int p;
+    cout << "Введіть будь ласка число p ";
+    cin >> p;
+    int* arr = new int[p];
+
+    for (int i = 0; i <= p; i++) {
+        cout << "arr[" << i << " ] ";
+        cin >> arr[i];
+    }
+
+    cout << "Створений масив для сортування: " << endl;
+    printarr(arr, p);
+    cout << endl;
+
+    bubble(arr, p);
+
+    cout << "Відсортований масив: " << endl;
+    printarr(arr, p);
+
+    delete[] arr;
+}
